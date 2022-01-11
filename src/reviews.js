e6e3359d44f0f749fe3fa20ff59405769f6dba16
chrisdfghjkl <cdepastene@gmail.com>
2022-01-11 00:33:14 -0800

    install and configure dotenv

--- src/reviews.js
+++ src/reviews.js
@@ -1,7 +1,7 @@
 export default class GetReview {  
   static async getReview(keyword) {
     try {
-      const response = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${keyword}&api-key=${process.env.API_KEY}
+      const response = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${keyword}&api-key=xbba5Ke4QWmLVrw6GSB65G07ReVqN6G6
       `);
       if (!response.ok) {
         throw Error(response.statusText);
